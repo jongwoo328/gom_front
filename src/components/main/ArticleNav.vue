a275f15cea2d552ee2121d1a8fedda9a7458434f
HwnagYoungJun <dudghkd4512@gmail.com>
2020-08-27 16:55:56 +0900

    make table

---
+++ src/components/main/ArticleNav.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="aricle-nav-box">
+		<div class="element"></div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="container">
+		<table>
+			<thead class="table-header">
+				<tr>
+					<th scope="col">번호</th>
+					<th scope="col">제목</th>
+					<th scope="col">글쓴이</th>
+					<th scope="col">날짜</th>
+					<th scope="col">스크랩</th>
+					<th scope="col">조회수</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="col">번호</th>
+					<th scope="col">ㅇㅇㅇㅇㅇㅇ</th>
+					<th scope="col">ㅇㅇㅇ</th>
+					<th scope="col">ㅇㅇㅇ</th>
+					<th scope="col">ㅇㅇㅇ</th>
+					<th scope="col">ㅇㅇㅇㅇㅇㅇㅇ</th>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
+
+<style lang="scss" scoped>
+.table-header {
+	border-top: 1px solid yellow;
+	border-bottom: 1px solid yellow;
+	background: green;
+}
+</style>
