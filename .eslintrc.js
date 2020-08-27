a35a5ebcb69d6d720ab3902e17b1fe7e0ab80dfc
HwnagYoungJun <dudghkd4512@gmail.com>
2020-08-27 11:58:26 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,26 @@ module.exports = {
   parserOptions: {
     parser: "babel-eslint"
   },
+  plugins: ['prettier'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-console": "off",
+    "prettier/prettier": ["error", {
+      singleQuote: true,
+      semi: true,
+      useTabs: true,
+      tabWidth: 2,
+      trailingComma: 'all',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'avoid',
+    }]
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+    }
+  ]
 };
